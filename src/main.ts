054ee50d5f40386a38ea4fad792f8bbb4d8d5c2b
Kuma-0x0 <yuta.kumagai.09@gmail.com>
2023-03-05 17:27:25 +0900

    [add]テキスト入力フォーム作成。

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,29 @@
 import { createApp, type Component } from 'vue';
 import App from './App.vue';
 
-import './assets/main.css';
+// import './assets/main.css';
 
 import 'vuetify/styles';
-import { createVuetify } from 'vuetify/lib/framework.mjs';
+import '@mdi/font/css/materialdesignicons.css';
+import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
+import { aliases, mdi } from 'vuetify/iconsets/mdi';
 
 const app = createApp(<Component>App);
 
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
 });
+
 app.use(vuetify);
 
 app.mount('#app');
