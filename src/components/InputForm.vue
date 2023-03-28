9b927e51462cefac05f9d3bf11b879c3c8677ab3
Kuma-0x0 <108416567+Kuma-0x0@users.noreply.github.com>
2023-03-28 21:40:53 +0900

    テキスト入力フォーム作成 (#2)
    * [add]vuetify追加。
    
    * [add]vuetifyを扱うコンポーネント追加。
    
    * [add]テキスト入力フォーム作成。
    
    * [update]コメントアウト削除

---
+++ src/components/InputForm.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const lastName = ref('');
+const firstName = ref('');
+const birthDay = ref(new Date(2000, 1, 1));
+const message = ref('');
+</script>
+<template>
+  <v-app>
+    <v-card title="入力欄">
+      <v-container>
+        <v-row>
+          <v-col align-self="start">
+            <v-text-field clearable label="姓" v-model="lastName"></v-text-field>
+            <v-label>{{ lastName }}</v-label>
+          </v-col>
+          <v-col align-self="start">
+            <v-text-field clearable label="名" v-model="firstName"></v-text-field>
+            <v-label>{{ firstName }}</v-label>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col align-self="start">
+            <v-text-field clearable label="誕生日" v-model="birthDay"></v-text-field>
+            <v-label>{{ birthDay }}</v-label>
+          </v-col>
+          <v-col align-self="start">
+            <v-text-field clearable label="メッセージ" wi v-model="message"></v-text-field>
+            <v-label>{{ message }}</v-label>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
+  </v-app>
+</template>
+
+<style scoped>
+.v-card {
+  width: 90%;
+  margin: 10px auto 0 auto;
+}
+
+.v-label {
+  margin-left: 16px;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,27 @@
 import { createApp, type Component } from 'vue';
 import App from './App.vue';
 
-import './assets/main.css';
+import 'vuetify/styles';
+import '@mdi/font/css/materialdesignicons.css';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import { aliases, mdi } from 'vuetify/iconsets/mdi';
 
 const app = createApp(<Component>App);
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+});
+
+app.use(vuetify);
+
 app.mount('#app');
